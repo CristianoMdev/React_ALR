2f1325d68cf8acb316486a4e5b007d6f8bf722cc
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-02-23 21:15:02 -0300

    Criando lista de categorias

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,40 +1,44 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
 
-constructor(){
-  super();
-  this.state = {
-    notas: []
-  }
-}
-
-  criarNota(titulo, texto){
-   const novaNota = {titulo, texto};
-   const novoArrayNotas = [...this.state.notas,novaNota]
-   const novoEstado = {
-     notas:novoArrayNotas
-   }
-   this.setState(novoEstado)
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    }
   }
 
-  deletarNota(index){
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  deletarNota(index) {
     let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
-    this.setState({notas:arrayNotas})
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas })
   }
 
   render() {
     return (
-      <section className="conteudo"> 
+      <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias />
+        </main>
         <ListaDeNotas
-        apagarNota={this.deletarNota.bind(this)}
-        notas={this.state.notas}/>
+          apagarNota={this.deletarNota.bind(this)}
+          notas={this.state.notas} />
       </section>
     );
   }
